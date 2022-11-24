21ca2b62c0b071943819c2ff52306f195f5ccaaf
Emilien Vaschalde <emilienvaschalde@yahoo.fr>
2022-11-24 13:18:05 +0100

    filters HomePage - Ui and Logic

---
+++ src/pages/Home/components/Filtres.tsx
@@ -0,0 +1,139 @@
+import { ICONS } from '../../../data/constants';
+
+type Filter = {
+    main: boolean,
+    res: boolean,
+    entrave: boolean,
+    terre: boolean,
+    feu: boolean,
+    eau: boolean,
+    air: boolean
+}
+
+interface Props {
+    filters: Filter,
+    setFilters: (value: any) => void
+}
+
+const Filtres = ({ filters, setFilters }: Props) => {
+    return (
+        <div className='homeFiltres'>
+            <div className='cont'>
+                <h2>Filtres</h2>
+                <div className='bloc'>
+                    <div className=' elem blue1'>
+                        <label htmlFor="main">
+                            <i className={ICONS.clipboard}></i>
+                            <p>Principales</p>
+                        </label>
+                        <input type="checkbox" id='main' 
+                            checked={filters.main} 
+                            onChange={() => {
+                                setFilters({
+                                    ...filters,
+                                    main: !filters.main
+                                })
+                            }}
+                        />
+                    </div>
+                    <div className=' elem black'>
+                        <label htmlFor="res">
+                            <i className={ICONS.shield}></i>
+                            <p>Res</p>
+                        </label>
+                        <input className='checkbox' type="checkbox" id='res' 
+                            checked={filters.res} 
+                            onChange={() => {
+                                setFilters({
+                                    ...filters,
+                                    res: !filters.res
+                                })
+                            }}
+                        />
+                    </div>
+                    <div className=' elem orange'>
+                        <label htmlFor="entrave">
+                            <i className={ICONS.lock}></i>
+                            <p>Entrave</p>
+                        </label>
+                        <input type="checkbox" id='entrave' 
+                            checked={filters.entrave} 
+                            onChange={() => {
+                                setFilters({
+                                    ...filters,
+                                    entrave: !filters.entrave
+                                })
+                            }}
+                        />
+                    </div>
+                </div>
+
+                <div className='bloc'>
+                    <div className='elem brown'>
+                        <label htmlFor="terre">
+                            <i className={ICONS.terre}></i>
+                            <p>Terre</p>
+                        </label>
+                        <input type="checkbox" id='terre'
+                            checked={filters.terre} 
+                            onChange={() => {
+                                setFilters({
+                                    ...filters,
+                                    terre: !filters.terre
+                                })
+                            }}
+                        />
+                    </div>
+                    <div className='elem red'>
+                        <label htmlFor="feu">
+                            <i className={ICONS.fire}></i>
+                            <p>Feu</p>
+                        </label>
+                        <input type="checkbox" id='feu' 
+                            checked={filters.feu} 
+                            onChange={() => {
+                                setFilters({
+                                    ...filters,
+                                    feu: !filters.feu
+                                })
+                            }}
+                        />
+                    </div>
+                    <div className='elem blue'>
+                        <label htmlFor="eau">
+                            <i className={ICONS.eau}></i>
+                            <p>Eau</p>
+                        </label>
+                        <input type="checkbox" id='eau' 
+                            checked={filters.eau} 
+                            onChange={() => {
+                                setFilters({
+                                    ...filters,
+                                    eau: !filters.eau
+                                })
+                            }}
+                        />
+                    </div>
+                    <div className='elem green'>
+                        <label htmlFor="air">
+                            <i className={ICONS.air}></i>
+                            <p>Air</p>
+                        </label>
+                        <input type="checkbox" id='air' 
+                            checked={filters.air} 
+                            onChange={() => {
+                                setFilters({
+                                    ...filters,
+                                    air: !filters.air
+                                })
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Filtres;
--- src/pages/Home/components/StatsBlocs.tsx
+++ src/pages/Home/components/StatsBlocs.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { ICONS } from '../../../data/constants';
+import Filtres from './Filtres';
 import StatsBlocElem from './StatsBlocElem';
 import StatsBlocEntrave from './StatsBlocEntrave';
 import StatsBlocMain from './StatsBlocMain';
@@ -6,22 +8,34 @@ import StatsBlocRes from './StatsBlocRes';
 import StatsBlocResDiv from './StatsBlocResDiv';
 
 const StatsBlocs = () => {
+    const [filters, setFilters] = useState(filterDefault)
     return (
         <div className='statsBloc-page'>
+            <Filtres filters={filters} setFilters={setFilters} />
             <div className='statsBloc-cont '>
-                <StatsBlocMain />
+                {filters.main && <StatsBlocMain />}
 
-                <StatsBlocElem elem={terre}/>
-                <StatsBlocElem elem={intel}/>
-                <StatsBlocElem elem={chance}/>
-                <StatsBlocElem elem={agi}/>
+                {filters.terre && <StatsBlocElem elem={terre} />}
+                {filters.feu && <StatsBlocElem elem={intel} />}
+                {filters.eau && <StatsBlocElem elem={chance} />}
+                {filters.air && <StatsBlocElem elem={agi} />}
 
-                <StatsBlocRes />
-                <StatsBlocEntrave />
+                {filters.res && <StatsBlocRes />}
+                {filters.entrave && <StatsBlocEntrave />}
             </div>
         </div>
     );
 };
+
+const filterDefault = {
+    main: true,
+    res: true,
+    entrave: true,
+    terre: true,
+    feu: true,
+    eau: true,
+    air: true
+}
 
 const terre = {
     name: "terre",
